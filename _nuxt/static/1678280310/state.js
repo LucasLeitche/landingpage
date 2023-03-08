61010be1bb0aa823e3416daf17f370f3e09063fc
lucasleitche <llneves019@gmail.com>
2023-03-08 09:58:32 -0300

    New Deployment

---
+++ _nuxt/static/1678280310/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flandingpage\u002F_nuxt\u002Fstatic\u002F1678280310",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Flandingpage\u002F",assetsPath:"\u002Flandingpage\u002F_nuxt\u002F",cdnURL:a}}}}(null));
